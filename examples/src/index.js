1f99843229de6e0e2b09ac9a7444067174019c96
arcthur <arthurtemptation@gmail.com>
2018-04-26 19:27:02 +0800

    fix: fix the example

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,12 +3,12 @@ import * as Mycache from 'mycache';
 const persist = new Mycache.Persist();
 
 async function test() {
-  await persist.setItem('a', { a: 1 }, 10);
-  await persist.setItem('b', { b: 1 }, new Date('January 3, 2013'));
+  await persist.set('a', { a: 1 }, 10);
+  await persist.set('b', { b: 1 }, new Date('January 3, 2013'));
 
-  setTimeout(async () => {
-    await persist.clearExpires();
-  }, 2000)
+  // setTimeout(async () => {
+  //   await persist.clearExpires();
+  // }, 2000)
 }
 
 test();
